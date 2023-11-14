1c088a0230705c8013a9faec0b4eb6ba81b93ea5
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2023-11-14 21:48:44 +0200

    the routing and skaffold is setup

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -5,7 +5,13 @@ const PORT = 3000;
 app.use(json())
 
 
+app.get('/api/users/currentUser', (req, res) => {
+    console.log('Request received in current User');
+
+    res.send('Hi there nice To meet You ...')
+})
+
 
 app.listen(PORT, () => {
-    console.log(`Running on port http://localhost:${PORT}`);
+    console.log(`Running on port http://localhost:${PORT} !!!`);
 })
